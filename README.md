34ee7e8c0843749e7090658552a1ad4e2089fd1d
Gabe Huerta <ghuerta@uoregon.edu>
2020-04-24 14:53:11 -0700

    Add Requirment 1: gallery.css gallery.html gallery.js

--- README.md
+++
@@ -1,7 +0,0 @@
-# Project 2 Repo
-
-Repo start-state:
-
-1. README.md
-2. .gitignore
-3. gallery.html, gallery.css, gallery.js
--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,15 +1,15 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function(){
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+    let prefixURL = "https://api.flickr.com/services/feeds/photos_public.gne?tags=";
     let suffixURL = "&format=json&jsoncallback=?";
     //get value entered by user from textbox
-    let flickrTag = $("input").???();
+    let flickrTag = $("input").val();
     let requestURL = prefixURL + flickrTag + suffixURL;
 
     //clear old photos
-    $(".photos").???("");
+    $(".photos").html("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
@@ -37,4 +37,14 @@ let main = function(){
 };
 
 
-$(document).ready(main);
+//$(document).ready(controller);
+
+
+//Register the controller after the DOM is complete
+window.addEventListener("load", function() {
+  //select the button
+  let button = document.querySelector("button");
+
+  //register the click handler for the button
+  button.addEventListener("click", controller);
+});
